b2f9ad211ce5fcee697234c91db191bfaf11dd44
kvakalvan <elshin95@bk.ru>
2022-04-19 20:54:19 +0300

    lesson 4.2 step 9

--- pages/locators.py
+++ pages/locators.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
+
 class LoginPageLocators():
-    CORRECT_URL ="https://selenium1py.pythonanywhere.com/ru/accounts/login/"
     CORRECT_LOGIN_FORM = (By.ID, "login_form")
     CORRECT_REGISTER_FORM = (By.ID, "register_form")
--- pages/login_page.py
+++ pages/login_page.py
@@ -11,11 +11,11 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
          # реализуйте проверку на корректный url адрес
-        assert "login" in self.browser.current_url
+        assert "login" in self.browser.current_url, 'неправильный url адрес '
 
     def should_be_login_form(self):
-        assert self.is_element_present(*LoginPageLocators.CORRECT_LOGIN_FORM)
+        assert self.is_element_present(*LoginPageLocators.CORRECT_LOGIN_FORM), "Не найдена форма для входа в аккаунт"
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
-        assert self.is_element_present(*LoginPageLocators.CORRECT_REGISTER_FORM)
+        assert self.is_element_present(*LoginPageLocators.CORRECT_REGISTER_FORM), " Не найдена форма регистрации"
